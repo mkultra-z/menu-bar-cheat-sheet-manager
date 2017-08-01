2bd3e8f7090aa798fc0edc8d2332e299e6a1b5d1
David Farrow <davidfarrow098@gmail.com>
2017-08-02 00:27:32 +1000

    Component styling

--- src/renderer/components/Templates/Command.vue
+++ src/renderer/components/Templates/Command.vue
@@ -14,20 +14,22 @@
   display: none; 
 }
 .command {
-  border-bottom: 1px solid #424242;
+  border-bottom: 1px solid #E6E6E6;
   padding-top: 1rem;
-  padding-bottom: 1.5rem;
+  padding-bottom: 1.2rem;
   max-width: 100%;
 }
 .command-code code {
   overflow-x:auto;
   white-space:nowrap;
-  background: #212121;
-  color: cornflowerblue;
-  font-size: 13px;  
+  background: #6A8EAE;
+  color: #8FF7C8;
+  font-size: 12px;
+  display: inline-block;
+  max-width: 100%;
 }
 .description {
-  color: white;
+  color: #6A8EAE;
   font-size: 13px;
   margin-bottom: .5rem;
 }
