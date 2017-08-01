cc0afcfaed637a110a08f232c8c37c1eee94713e
David Farrow <davidfarrow098@gmail.com>
2017-08-01 20:51:42 +1000

    Added bootstrap

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import axios from 'axios'
+import BootstrapVue from 'bootstrap-vue'
 
 import App from './App'
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
+Vue.use(BootstrapVue)
 
 /* eslint-disable no-new */
 new Vue({
