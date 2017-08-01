4e6231f2fa22d92021cd244aeaf70d877f41aaad
David Farrow <davidfarrow098@gmail.com>
2017-08-01 20:55:42 +1000

    Command template component

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,20 +1,58 @@
 <template>
   <div id="app">
-    <landing-page></landing-page>
+    <navigation></navigation>
+    <command v-for="(command, index) in commands" 
+      v-bind:command="command" 
+      v-bind:index="index"></command>
   </div>
 </template>
 
 <script>
-  import LandingPage from '@/components/LandingPage'
+  import Navigation from '@/components/Navigation'
+  import Command from '@/components/Templates/Command'
+  import 'bootstrap/dist/css/bootstrap.css'
+  import 'bootstrap-vue/dist/bootstrap-vue.css'
 
   export default {
     name: 'menu-bar-cheat-sheet-manager',
     components: {
-      LandingPage
+      Navigation,
+      Command
+    },
+    data () {
+      return {
+        commands: [
+          {
+            command: 'git clone <remote> -b <branch>',
+            description: 'Clone a remote branch'
+          },
+          {
+            command: 'git pull <remote> -b <branch>',
+            description: 'Pull the latest code from a remote branch'
+          },
+          {
+            command: 'git commit -a -m "commit message"',
+            description: 'Creates a commit with all the staged files'
+          }
+        ]
+      }
     }
   }
 </script>
 
 <style>
-  /* CSS */
+/* CSS */
+/* .html {
+  max-width: 325;
+  max-height: 400;
+  overflow: hidden;
+  white-space: none;
+} */
+body {
+  background: #323232;
+}
+.container {
+margin: 0;
+padding: 0;
+}
 </style>
---
+++ src/renderer/components/Templates/Command.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container row command">
+    <div class="col-12 description">
+      {{command.description}}
+    </div>
+    <div class="col-12 command-code">
+      <code>{{command.command}}</code>
+    </div>
+  </div>
+</template>
+
+<style>
+.command-code code::-webkit-scrollbar { 
+  display: none; 
+}
+.command {
+  border-bottom: 1px solid #424242;
+  padding-top: 1rem;
+  padding-bottom: 1.5rem;
+  max-width: 100%;
+}
+.command-code code {
+  overflow-x:auto;
+  white-space:nowrap;
+  background: #212121;
+  color: cornflowerblue;
+  font-size: 13px;  
+}
+.description {
+  color: white;
+  font-size: 13px;
+  margin-bottom: .5rem;
+}
+</style>
+
+
+<script>
+  export default {
+    props: ['command', 'index']
+  }
+</script>
